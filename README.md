4714a73802aaffa33a11d6d2541740ea61edab6e
Gouaby Kalivogui <gouabyk14@yahoo.fr>
2022-08-24 02:18:58 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# NFT Marketplace
+
+## Technology Stack & Tools
+
+- Solidity (Writing Smart Contract)
+- Javascript (React & Testing)
+- [Ethers](https://docs.ethers.io/v5/) (Blockchain Interaction)
+- [Hardhat](https://hardhat.org/) (Development Framework)
+- [Ipfs](https://ipfs.io/) (Metadata storage)
+- [React routers](https://v5.reactrouter.com/) (Navigational components)
+- Tailwing CSS
+
+## Setting Up
+### 1. Clone/Download the Repository
+
+### 2. Install Dependencies:
+```
+$ cd nft_marketplace
+$ npm install
+```
+ 
+
+### 3. Launch 
+`$ npm run start`
+
+License
+----
+MIT
---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+require('dotenv').config()
+
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
